afa19c44443feee52efbbc68bbfab6a7ba169085
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-11-17 15:27:24 +0900

    add files

---
+++ 2023_11_17_js/my.js
@@ -0,0 +1,20 @@
+//jQueryの作法
+$(function(){
+  //キャンバスの要素を取得
+  var can = $("#can")
+  //コンテキストを取得
+  var ctx = can[0].getContext("2d");
+
+  //矩形の描画
+  ctx.strokeRect(100, 100, 440, 280);
+
+  //for文で矩形の中に円を描画
+  for (var j = 0; j < 7; j++) {
+    for (var i = 0; i < 11; i++) {
+      ctx.beginPath();
+      ctx.ellipse(120 + i * 40, 120+j*40, 20, 20, Math.PI / 4, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+
+  });
