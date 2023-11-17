93b8c99765c6740989c0e96d0141698e79af6d1f
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-11-17 14:40:54 +0900

    fix

--- 2023:11:8_js/my.js
+++ 2023:11:8_js/my.js
@@ -2,6 +2,3 @@ alert('まだ途中です');
 function add(a,b){
     return a+b;
 }
-
-var span = $("#sum");
-span.text(add(2,3));
--- 2023:11:8_js2/my.js
+++ 2023:11:8_js2/my.js
@@ -1,9 +1,10 @@
 $(function(){
-        alert('もう読み込みました');
-        function add(a,b){
-            return a+b;
-        }
-});
 
-var span = $("#sum");
-span.text(add(2,3));
+    alert("もう読み込みました。");
+    function add(a,b){
+      return a+b;
+    }
+  
+    var span=$("#sum");
+    span.text(add(2,3));
+  });
