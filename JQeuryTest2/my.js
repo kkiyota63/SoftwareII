9afc2ee56f16ebebabc9463aa8c521d9859db667
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-12-06 13:04:39 +0900

    fix

--- JQeuryTest2/my.js
+++ JQeuryTest2/my.js
@@ -1,19 +1,34 @@
 $(function() {
-    var isDog = true; // 状態を追跡するフラグ
+  var isDog = true; // 現在の画像が犬かどうかを追跡
+  var originalSrc = "dog1.jpg"; // 元の画像のソース
 
-    // イベントハンドラ
-    var f = function(ev) {
-        if (isDog) {
-            $("#im_001").attr("src", "cat1.jpg"); // 猫の画像に変更
-            $("#h2_001").text("猫の画像");
-            isDog = false; // フラグを猫の状態に更新
-        } else {
-            $("#im_001").attr("src", "dog1.jpg"); // 犬の画像に戻す
-            $("#h2_001").text("犬の画像");
-            isDog = true; // フラグを犬の状態に更新
-        }
-    }
+  // ボタンのクリックイベントハンドラ
+  var toggleImage = function() {
+      if (isDog) {
+          $("#im_001").attr("src", "cat1.jpg"); // 猫の画像に変更
+          $("#h2_001").text("猫の画像");
+          originalSrc = "cat1.jpg"; // 元の画像ソースを更新
+          isDog = false;
+      } else {
+          $("#im_001").attr("src", "dog1.jpg"); // 犬の画像に戻す
+          $("#h2_001").text("犬の画像");
+          originalSrc = "dog1.jpg"; // 元の画像ソースを更新
+          isDog = true;
+      }
+  }
 
-    // イベント登録
-    $("#bt_001").click(f);
+  // 画像にマウスが乗ったときのイベントハンドラ
+  var showPanda = function() {
+      $("#im_001").attr("src", "panda1.jpg"); // パンダの画像に変更
+      $("#h2_001").text("パンダの画像");
+  }
+
+  // 画像からマウスが離れたときのイベントハンドラ
+  var showOriginal = function() {
+      $("#im_001").attr("src", originalSrc); // 元の画像に戻す
+  }
+
+  // イベントの登録
+  $("#bt_001").click(toggleImage);
+  $("#im_001").mouseenter(showPanda).mouseleave(showOriginal);
 });
