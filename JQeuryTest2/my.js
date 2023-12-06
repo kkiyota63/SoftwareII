fb7ac6d341eaa6a27aee8097d098e778308e0c55
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-12-06 12:56:30 +0900

    push

--- JQeuryTest2/my.js
+++ JQeuryTest2/my.js
@@ -1,13 +1,19 @@
-//jQueryの作法
-$(function(){
+$(function() {
+    var isDog = true; // 状態を追跡するフラグ
 
-  // イベントハンドラ
-  var f=function(ev){
-    $("#im_001").attr("src","cat1.jpg");
-    $("#h2_001").text("猫の画像");
-  }
+    // イベントハンドラ
+    var f = function(ev) {
+        if (isDog) {
+            $("#im_001").attr("src", "cat1.jpg"); // 猫の画像に変更
+            $("#h2_001").text("猫の画像");
+            isDog = false; // フラグを猫の状態に更新
+        } else {
+            $("#im_001").attr("src", "dog1.jpg"); // 犬の画像に戻す
+            $("#h2_001").text("犬の画像");
+            isDog = true; // フラグを犬の状態に更新
+        }
+    }
 
-  // イベント登録
-  $("#bt_001").click(f);
-
+    // イベント登録
+    $("#bt_001").click(f);
 });
